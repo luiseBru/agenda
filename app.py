724e2cbe6115966e84ff1d9ea74b7e22664e914a
luiseBru <bruna.l.silva28@aluno.senai.br>
2026-01-27 15:28:42 -0300

    feat: front-end com funcionalidade basica

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from models.tarefa import Tarefa
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html', titulo='home')
+
+@app.route('/agenda', methods=['GET', 'POST'])
+def agenda():
+    tarefa = None
+
+    if request.method == 'POST':
+        titulo_tarefa = request.form['titulo-tarefa']
+        data_conclusao = request.form['data-conclusao']
+        tarefa = Tarefa(titulo_tarefa, data_conclusao)
+
+    return render_template('agenda.html', titulo='agenda', tarefa=tarefa)
+
+@app.route('/ola')
+def ola_mundo():
+    return "Ola, mundo"
---
+++ models/tarefa.py
@@ -0,0 +1,4 @@
+class Tarefa:
+    def __init__(self, titulo_tarefa, data_conclusao=None):
+        self.titulo_tarefa = titulo_tarefa
+        self.data_conclusao = data_conclusao
